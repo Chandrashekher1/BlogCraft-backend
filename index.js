d80a1417ff4e4f6b5380406cc703e64576009ffd
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-03-25 08:43:09 +0530

    pass jwtPrivateKey error check

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose')
 const express = require('express')
 const app = express()
-
+require('dotenv').config();
 require('./startup/routes')(app)
+
+if(!process.env.post_jwtPrivateKey) {
+    console.error('FATAL ERROR: jwtPrivateKey is not defined.')
+    process.exit(1)
+}
 
 mongoose.connect('mongodb://localhost/Blog')
     .then(() => console.log("mongodb is connected..."))

9e713104004f23c3a3ae8565b9887eadc4568c4b
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-03-28 16:57:39 +0530

    change cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ if(!process.env.post_jwtPrivateKey) {
     process.exit(1)
 }
 
-cors.use(cors({
+app.use(cors({
     origin:"http://localhost:5173/",
     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
     allowedHeaders: ['Content-Type', 'Authorization'], 

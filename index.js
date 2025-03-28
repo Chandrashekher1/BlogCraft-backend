505ad476f6a995c6dae0cd9ddf3434ecce20d927
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-03-28 18:24:00 +0530

    change origin of cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ if(!process.env.post_jwtPrivateKey) {
 }
 
 app.use(cors({
-    origin:"http://localhost:5173/",
+    origin:"http//localhost:5173",
     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
     allowedHeaders: ['Content-Type', 'Authorization'], 
 }))

ecf349c0d77b441da27b8f37f9944ec87723efdf
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-03-30 10:34:07 +0530

    remove admin

--- routes/posts.js
+++ routes/posts.js
@@ -16,7 +16,7 @@ router.get('/:id',auth, async (req,res) => {
     res.send(post)
 })
 
-router.post('/',[auth,admin], async (req,res) => {
+router.post('/',auth, async (req,res) => {
     const {error} = validate(req.body)
     if(error) return res.status(400).send(error.details[0].message)
 

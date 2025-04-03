51be9f9f316628c58a96db9d4e1568d6ba1e0c60
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-04-03 10:59:51 +0530

    allow get post by id

--- routes/posts.js
+++ routes/posts.js
@@ -14,15 +14,15 @@ router.get('/', async (req, res) => {
     }
 });
 
-// router.get('/:id', auth, async (req, res) => {
-//     try {
-//         const post = await Posts.findById(req.params.id).populate('userId', 'name email');
-//         if (!post) return res.status(404).send("Blog not found...");
-//         res.send(post);
-//     } catch (error) {
-//         res.status(500).send("Internal Server Error");
-//     }
-// });
+router.get('/:id', auth, async (req, res) => {
+    try {
+        const post = await Posts.findById(req.params.id).populate('userId', 'name email');
+        if (!post) return res.status(404).send("Blog not found...");
+        res.send(post);
+    } catch (error) {
+        res.status(500).send("Internal Server Error");
+    }
+});
 
 router.post('/', auth, async (req, res) => {
     console.log("Decoded User:", req.user);

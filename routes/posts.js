6c20fbc40bb137ab2bfec5b9a6a87e790234f99b
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-06-13 16:46:35 +0530

    add image path

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,9 @@
-const admin = require('../middleware/admin');
-const auth = require('../middleware/auth');
-const { Posts, validate } = require('../models/posts');
-const express = require('express');
-const router = express.Router();
-const mongoose = require('mongoose');
+const uploadMultiple = require('../config/storage')
+const auth = require('../middleware/auth')
+const { Posts, validate } = require('../models/posts')
+const express = require('express')
+const router = express.Router()
+const mongoose = require('mongoose')
 
 router.get('/', async (req, res) => {
     try {
@@ -39,27 +39,29 @@ router.get('/user/:userId', auth, async (req, res) => {
         res.status(500).send("Internal Server Error");
     }
 });
-router.post('/', auth, async (req, res) => {
+router.post('/', auth, uploadMultiple,async (req, res) => {
     try {
-        const { error } = validate(req.body);
-        if (error) return res.status(400).send(error.details[0].message);
-
+        const images = req.files?.map((image) => image.path)
+        const { error } = validate(req.body)
+        if (error) return res.status(400).send(error.details[0].message)
+        
         let post = new Posts({
             title: req.body.title,
             content: req.body.content,
             author: req.body.author,
+            image: images,
             userId: req.user ? req.user._id : null
-        });
-
-        post = await post.save();
-        res.send(post);
+        })
+        post = await post.save()
+        res.send(post)
     } catch (error) {
         res.status(500).send("Internal Server Error");
     }
 });
 
-router.put('/:id', auth, async (req, res) => {
+router.put('/:id', auth,uploadMultiple, async (req, res) => {
     try {
+        const images = req.files?.map((image) => image.path)
         const { error } = validate(req.body);
         if (error) return res.status(400).send(error.details[0].message);
 
@@ -69,6 +71,7 @@ router.put('/:id', auth, async (req, res) => {
                 title: req.body.title,
                 author: req.body.author,
                 content: req.body.content,
+                image: images
             },
             { new: true } 
         );
@@ -78,7 +81,27 @@ router.put('/:id', auth, async (req, res) => {
     } catch (error) {
         res.status(500).send("Internal Server Error");
     }
-});
+})
+
+router.patch('/:id',auth, uploadMultiple, async (req, res) => {
+    const images = req.files?.map((image) => image.path)
+    try {
+        const post_update = await Posts.findByIdAndUpdate(
+            req.params.id,
+            {
+                title: req.body.title,
+                author: req.body.author,
+                content: req.body.content,
+                image: images
+            },
+            { new: true } 
+        )
+        if (!post_update) return res.status(404).send("Blog not found...");
+        res.send(post_update);
+    } catch (error) {
+        res.status(500).send("Internal Server Error");
+    }
+})
 
 router.delete('/:id', auth, async (req, res) => {
     try {
